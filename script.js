0ce88eafab3bfcf56e34fa8d41a83cf637f959b4
mahsaabdolahnezhad <141028214+mahsaabdolahnezhad@users.noreply.github.com>
2023-08-03 15:24:13 +0330

    added table and description

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ let week = [
 let day = week[now.getDay()];
 let weekdays = document.querySelector(".time");
 weekdays.innerHTML = `${day} ${now.getHours()}:${now.getMinutes()}`;
+// counting rows 
+let n ; 
+//measring time 
+let time ; 
+let description = document.querySelector("#description");
+description.innerHTML = `created ${n} records at ${time}`; 
